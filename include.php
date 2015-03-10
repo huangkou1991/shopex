afda7a169bf82fdf0444b951504bc6bd48bac3a2
huangkou <1249868674@qq.com>
2015-03-10 13:54:47 +0800

    git commit

--- include.php
+++ include.php
@@ -1,3 +1,13 @@
 <?
-  phpinfo();
+	function lock_write($file){
+		if(!file_exists($file)) return false;
+		$handle = fopen($file, '+w');
+		if(flock($handle, LOCK_EX)){
+			fwrite($handle, '$string');
+			flock($handle, LOCK_UN);
+		}else{
+			echo 'file is locking';
+		}
+		fclose($handle);
+	}
 ?>
